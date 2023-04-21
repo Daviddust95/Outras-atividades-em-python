3085ea505a89a9fe1087db8255909e25cc3d1b30
Alisson David de Melo <124353154+Daviddust95@users.noreply.github.com>
2023-04-21 12:30:31 -0300

    Add files via upload
    Os documentos desta pasta serão atualizados periodicamente, evidenciando o meu nível de conhecimento em Python.

---
+++ Atividade 2 aula 4 nasc mês e ano.py
@@ -0,0 +1,4 @@
+dia = input ("Qual o dia do seu nascimento ?")
+mes =input ("Qual mês você nasceu ?")
+ano =input ("Qual ano você nasceu ?")
+print (f"sua data de nascimento é:, {dia}, {mes}, {ano}, correto ?")
---
+++ Atividae 3 aula 4 soma simples.py
@@ -0,0 +1,5 @@
+
+num1 = int(input("informe um numero:"))
+num2 = int(input("Digite outro numero:"))
+
+print(f"A soma dos dois números é {num1 + num2}")
